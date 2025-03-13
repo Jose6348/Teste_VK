781e24625a25257bdd34c06a637f1a038b609ef5
Jose6348 <jfalasco88@gmail.com>
2025-03-13 13:46:20 -0300

    feat: add main page components

---
+++ src/components/SalesPage.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="min-h-screen">
+    <Header />
+    <Hero />
+    <Sobre />
+    <QuemEVeveFit />
+    <MetodoBarrigaNegativa />
+    <InformativoBarriga />
+    <TreinosDiarios />
+    <Beneficios />
+    <Galeria />
+    <Depoimentos />
+    <Planos />
+    <Contato />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from './sales-page/Header.vue'
+import Hero from './sales-page/Hero.vue'
+import Sobre from './sales-page/Sobre.vue'
+import QuemEVeveFit from './sales-page/QuemEVeveFit.vue'
+import MetodoBarrigaNegativa from './sales-page/MetodoBarrigaNegativa.vue'
+import InformativoBarriga from './sales-page/InformativoBarriga.vue'
+import TreinosDiarios from './sales-page/TreinosDiarios.vue'
+import Beneficios from './sales-page/Beneficios.vue'
+import Galeria from './sales-page/Galeria.vue'
+import Depoimentos from './sales-page/Depoimentos.vue'
+import Planos from './sales-page/Planos.vue'
+import Contato from './sales-page/Contato.vue'
+import Footer from './sales-page/Footer.vue'
+
+export default {
+  name: 'SalesPage',
+  components: {
+    Header,
+    Hero,
+    Sobre,
+    QuemEVeveFit,
+    MetodoBarrigaNegativa,
+    InformativoBarriga,
+    TreinosDiarios,
+    Beneficios,
+    Galeria,
+    Depoimentos,
+    Planos,
+    Contato,
+    Footer
+  }
+}
+</script> 
